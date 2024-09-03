95afc613b301440266ff4945e88205153dd63728
MadGenius16 <aleksandrco777@gmail.com>
2024-09-03 14:59:13 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -4,32 +4,32 @@ axios.defaults.baseURL = "https://api.themoviedb.org/3";
 const apiKey = "d969496d1bc07cbd028cf70601276a53"
 
 export const getTrendFilms = async () => {
-  const response  = await axios.get(`/trending/movie/day?api_key=${apiKey}`);
-  return response.data;
+  const {data}  = await axios.get(`/trending/movie/day?api_key=${apiKey}`);
+  return data;
 };
 
 export const getFilmsById = async (filmId) => {
-  const response = await axios.get(`/movie/${filmId}?api_key=${apiKey}`);
-  return response.data;
+  const {data} = await axios.get(`/movie/${filmId}?api_key=${apiKey}`);
+  return data;
 };
 
 export const getFilmReviews = async (filmId) => {
-  const response = await axios.get(
+  const {data} = await axios.get(
     `/movie/${filmId}/reviews?api_key=${apiKey}`
   );
-  return response.data;
+  return data;
 };
 
 export const getCastList = async (filmId) => {
-  const response = await axios.get(
+  const {data} = await axios.get(
     `/movie/${filmId}/credits?api_key=${apiKey}`
   );
-  return response.data;
+  return data;
 };
 
 export const getFilmByKeyword = async (query) => {
-  const response = await axios.get(
+  const {data} = await axios.get(
     `/search/movie?api_key=${apiKey}&query=${query}`
   );
-  return response.data;
+  return data;
 };
