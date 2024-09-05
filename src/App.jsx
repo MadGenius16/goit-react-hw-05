abffb2c2d9a047733b346500299bd93518281763
MadGenius16 <aleksandrco777@gmail.com>
2024-09-05 11:47:22 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import css from "./App.module.css"
 
-const Loader = lazy(() => import("../src/сomponents/Loader/Loader.jsx"))
-const HomePage = lazy(() => import("../src/pages/HomePage/HomePage.jsx"))
+
 const MoviesPage = lazy(() => import("../src/pages/MoviesPage/MoviesPage.jsx"))
 const MovieDetailsPage = lazy(() => import("../src/pages/MovieDetailsPage/MovieDetailsPage.jsx"))
 const NotFoundPage = lazy(() => import("../src/pages/NotFoundPage/NotFoundPage.jsx"))
 const Navigation = lazy(() => import("../src/сomponents/Navigation/Navigation.jsx")) 
 const MovieCast = lazy(() => import("../src/сomponents/MovieCast/MovieCast.jsx")) 
 const MovieReviews = lazy(() => import("../src/сomponents/MovieReviews/MovieReviews.jsx"))
-
+const HomePage = lazy(() => import("../src/pages/HomePage/HomePage.jsx"))
+const Loader = lazy(() => import("../src/сomponents/Loader/Loader.jsx"))
 function App() {
   
+
   return (
     <div className={css.container}>
       <Navigation/>
